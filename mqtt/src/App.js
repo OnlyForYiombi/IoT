d4680f6729d4a5b093c8545dce7b416a82c07c87
mynameisgithub@gmail.com <onlyforgithub1@gmail.com>
2025-05-09 02:19:34 +0900

    setupfile

---
+++ mqtt/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ValveControl from './ValveControl';
+
+function App() {
+  return (
+    <div>
+      <ValveControl />
+    </div>
+  );
+}
+
+export default App;
