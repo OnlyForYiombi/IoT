0b2cf687cb72f3599f6a05edd199e4f013e809e4
mynameisgithub@gmail.com <onlyforgithub1@gmail.com>
2025-05-09 02:23:23 +0900

    Update MQTT_code.ino
    왼쪽 오른쪽 구분 가능
    LED on off 기능 구현

--- mqtt/MQTT_code/MQTT_code.ino
+++ mqtt/MQTT_code/MQTT_code.ino
@@ -1,9 +1,12 @@
 #include <WiFi.h>
+#include <WebServer.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
 #include <WiFiClientSecure.h> 
 #include <PubSubClient.h>
-
-const char* ssid = "NTH414"; // 포항 도시숲 WiFi 대체
-const char* password = "cseenth414"; 
+// WiFi Configuration 
+const char* ssid = "NTH414";
+const char* password = "cseenth414";
 
 // MQTT Broker 정보
 const char* mqtt_server = "6a862c626ea84addad97efb3a08f02de.s1.eu.hivemq.cloud"; 
@@ -12,7 +15,60 @@ const int mqtt_port = 8883; // TLS 포트
 WiFiClientSecure espClient;   //  Secure로 변경
 PubSubClient client(espClient);
 
-#define VALVE_PIN  13   // D13 핀 사용
+// OLED Configuration
+#define SCREEN_WIDTH    128
+#define SCREEN_HEIGHT   64
+#define OLED_RESET      -1
+#define OLED_ADDR       0x3C
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+// Valve configuration 
+#define VALVE_PIN1    25  // LEFT
+#define VALVE_PIN2    21  // RIGHT
+
+
+void mqttCallback(char* topic, byte* payload, unsigned int length) {
+  String message;
+  for (unsigned int i = 0; i < length; i++) {
+    message += (char)payload[i];
+  }
+
+  Serial.println("[MQTT] Message arrived [" + String(topic) + "] : " + message);
+  
+  if(String(topic) == "valve/1/right") {
+    if(message == "ON"){
+      digitalWrite(VALVE_PIN2, HIGH);
+      Serial.println("[VALVE1-RIGHT] ON");
+    } else if(message == "OFF"){
+      digitalWrite(VALVE_PIN2, LOW);
+      Serial.println("[VALVE1-RIGHT] OFF");
+    }
+  } else if(String(topic) == "valve/1/left") {
+    if(message == "ON"){
+      digitalWrite(VALVE_PIN1, HIGH);
+      Serial.println("[VALVE1-LEFT] ON");
+    } else if(message == "OFF"){
+      digitalWrite(VALVE_PIN1, LOW);
+      Serial.println("[VALVE1-LEFT] OFF");
+    }
+  }
+}
+
+void reconnect() {
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    if (client.connect("Lora32_Client","Yiombi","Aa12345678")) {
+      Serial.println("connected");
+      client.subscribe("valve/1/right");
+      client.subscribe("valve/1/left");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      delay(5000);
+    }
+  }
+}
 
 void setup_wifi() {
   Serial.println();
@@ -36,48 +92,17 @@ void setup_wifi() {
   Serial.println("IP address: ");
   Serial.println(WiFi.localIP());
 
-  // ✅ SSL 인증서 검증 OFF
   espClient.setInsecure(); 
 }
 
-void mqttCallback(char* topic, byte* payload, unsigned int length) {
-  String message;
-  for (unsigned int i = 0; i < length; i++) {
-    message += (char)payload[i];
-  }
-  Serial.println("[MQTT] Message arrived [" + String(topic) + "] : " + message);
-
-  if (String(topic) == "valve/1/control/right") {
-    if (message == "ON") {
-      digitalWrite(VALVE_PIN, HIGH);
-      client.publish("valve/1/status", "ON");
-    } else if (message == "OFF") {
-      digitalWrite(VALVE_PIN, LOW);
-      client.publish("valve/2/status", "OFF");
-    }
-  }
-}
-
-void reconnect() {
-  while (!client.connected()) {
-    Serial.print("Attempting MQTT connection...");
-    if (client.connect("Lora32_Client","Yiombi","Aa12345678")) {
-      Serial.println("connected");
-      client.subscribe("valve/1/control");
-    } else {
-      Serial.print("failed, rc=");
-      Serial.print(client.state());
-      Serial.println(" try again in 5 seconds");
-      delay(5000);
-    }
-  }
-}
 
 void setup() {
-  pinMode(VALVE_PIN, OUTPUT); 
-  digitalWrite(VALVE_PIN, LOW);
-
   Serial.begin(115200);
+  pinMode(VALVE_PIN1, OUTPUT);
+  pinMode(VALVE_PIN2, OUTPUT);
+  digitalWrite(VALVE_PIN1, LOW);
+  digitalWrite(VALVE_PIN2, LOW);
+
   setup_wifi();
   client.setServer(mqtt_server, mqtt_port);
   client.setCallback(mqttCallback);
@@ -89,3 +114,4 @@ void loop() {
   }
   client.loop();
 }
+
